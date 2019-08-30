68dabf9df6f0d4762a0585e8073e39c9390f9b5c
Alireza Seddighi <seddighi.alireza78@gmail.com>
2019-08-30 23:26:36 +0430

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -12,37 +12,41 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('telegram_bot_api');
-        $rootNode = $treeBuilder->root('telegram_bot_api');
+    	if (Kernel::VERSION_ID >= 40200) {
+            $builder = new TreeBuilder('telegram_bot_api');
+        } else {
+            $builder = new TreeBuilder();
+        }
+        $rootNode = \method_exists($builder, 'getRootNode') ? $builder->getRootNode() : $builder->root('telegram_bot_api');
 
-	    $rootNode
-            ->children()
-                ->arrayNode('bots')
-                    ->useAttributeAsKey('name')
-                    ->prototype('array')
-                        ->children()
-                            ->scalarNode( 'token' )->isRequired()->end()
-                        ->end()
-                    ->end()
-                ->end()
-                ->scalarNode('default')->defaultNull()->end()
-                ->scalarNode('proxy')->defaultNull()->end()
-                ->booleanNode('async_requests')->defaultFalse()->end()
-                ->arrayNode('development')
-                    ->children()
-                        ->arrayNode('developers_id')->prototype('scalar')->end()
-                    ->end()
-                        ->arrayNode('maintenance')
-                            ->children()
-                                ->booleanNode('enable')->defaultFalse()->end()
-                                ->scalarNode('text')->defaultValue('The robot is being repaired! Please come back later.')->end()
-                            ->end()
-                        ->end()
-                    ->end()
-                ->end()
+	$rootNode
+	    	->children()
+		->arrayNode('bots')
+		    ->useAttributeAsKey('name')
+		    ->prototype('array')
+			->children()
+			    ->scalarNode( 'token' )->isRequired()->end()
+			->end()
+		    ->end()
+		->end()
+		->scalarNode('default')->defaultNull()->end()
+		->scalarNode('proxy')->defaultNull()->end()
+		->booleanNode('async_requests')->defaultFalse()->end()
+		->arrayNode('development')
+		    ->children()
+			->arrayNode('developers_id')->prototype('scalar')->end()
+		    ->end()
+			->arrayNode('maintenance')
+			    ->children()
+				->booleanNode('enable')->defaultFalse()->end()
+				->scalarNode('text')->defaultValue('The robot is being repaired! Please come back later.')->end()
+			    ->end()
+			->end()
+		    ->end()
+		->end()
         ->end();
 
-        return $treeBuilder;
+        return $builder;
     }
 
 }
